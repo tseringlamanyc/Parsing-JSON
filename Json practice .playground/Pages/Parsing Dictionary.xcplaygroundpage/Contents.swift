8dae6f3dad8c6b0042e4e2f206675967d1a4f67a
Tsering Lama <tseringglamaa@gmail.com>
2020-11-03 19:36:15 -0500

    completed parsing json from dictionary

--- Json practice .playground/Pages/Parsing Dictionary.xcplaygroundpage/Contents.swift
+++ Json practice .playground/Pages/Parsing Dictionary.xcplaygroundpage/Contents.swift
@@ -1,3 +1,45 @@
-import Foundation
+import UIKit
 
-// PARSING DICTIONARY 
+// PARSING DICTIONARY
+
+// convert json into data
+
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+//MARK:- CREATE MODEL
+
+// Codable : Decode and Encode
+// Decode: converts json data
+// Encode: coverts to json data (post)
+
+struct ResultsWrapper: Decodable {
+    let results : [Contact]
+}
+
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+//MARK:- DECODE
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results // array of contacts
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
